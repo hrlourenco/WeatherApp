04e06889f62d0b3ef937338ee6a1735a75e3fbb1
João Faria <jfaria.gmr@gmail.com>
2016-09-15 01:24:50 +0100

    Beach details final

--- app/src/main/java/mei/weatherapp/BeachDetails.java
+++ app/src/main/java/mei/weatherapp/BeachDetails.java
@@ -3,8 +3,11 @@ package mei.weatherapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import mei.weatherapp.adapter.forecastAdapter;
 import mei.weatherapp.asynctasks.GetImageAsync;
@@ -14,7 +17,10 @@ public class BeachDetails extends AppCompatActivity {
 
   private ImageView imgView;
   private ListView lv_forecast;
+  private TextView tv_name;
+  private CheckBox cb_fav;
   private Context ctx;
+  private Praia praia;
 
 
   @Override
@@ -23,12 +29,32 @@ public class BeachDetails extends AppCompatActivity {
     setContentView(R.layout.activity_beachdetails);
 
     imgView = (ImageView) findViewById(R.id.imgFoto);
-    Praia praia = (Praia) this.getIntent().getSerializableExtra("praia");
+    praia = (Praia) this.getIntent().getSerializableExtra("praia");
 
     GetImageAsync getImage = new GetImageAsync(imgView);
     getImage.execute(praia.getNome());
 
     lv_forecast = (ListView) findViewById(R.id.lv_forecast);
     lv_forecast.setAdapter(new forecastAdapter(BeachDetails.this, praia.getForecast()));
+
+    tv_name = (TextView) findViewById(R.id.tv_name);
+    tv_name.setText(praia.getNome());
+
+    cb_fav = (CheckBox) findViewById(R.id.cb_fav);
+    cb_fav.setChecked(praia.getFavorita()==0 ? Boolean.FALSE : Boolean.TRUE);
+  }
+
+  public void onCheckboxClicked(View view) {
+    boolean checked = ((CheckBox) view).isChecked();
+
+    switch(view.getId()) {
+      case R.id.cb_fav:
+        if (checked){
+          praia.setFavorita(1);
+        }else{
+          praia.setFavorita(0);
+        }
+        break;
+    }
   }
 }
